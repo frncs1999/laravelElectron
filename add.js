ff3c36dedba58b430940d20c7c2fc14700439de5
frncs1999 <francis.resugenio@davidvenneit.com>
2020-09-08 16:31:02 +0800

    init

--- add.js
+++ add.js
@@ -1,8 +1,7 @@
 const axios = require('axios')
 
-
+const {getCurrentWindow} = require('electron').remote;
 formAdd.addEventListener('click', function() { 
-
     var productname = document.getElementById('productname');
     var productdes = document.getElementById('productdes')
 
@@ -10,9 +9,6 @@ formAdd.addEventListener('click', function() {
 
     bodyFormData.append('productname', productname.value);
     bodyFormData.append('productdes', productdes.value);
-
-    console.log(productname.value)
-    console.log(productdes.value)
 
     axios({
         method: 'post',
@@ -22,30 +18,96 @@ formAdd.addEventListener('click', function() {
         enctype: "multipart/form-data"
         })
 
-        .then(function (response) {
-
-            var json = JSON.stringify(eval(response.data));
-            console.log(json);
+        .then(function (response) {             
+            console.log(response.data.message)
+            getCurrentWindow().reload()
   
-            if (json == `{"auth":true}`) {
-              console.log('SUCCESS');
-  
-              
-              // import { prodwin } from './win.js';
-              // prodwin();
-  
-              const myModule = require('./win');
-              myModule.prodwin();
-            
-              
-            }
-          
-
         })
         .catch(function (error) {
 
             console.log(error);
         });
 
+        
 
 });
+
+
+$( document ).ready(function() {
+    $("#formUpd").hide();
+
+    axios.get('http://localhost:8000/api/product/index')
+    .then((response) => {
+
+      var prod = response.data;
+
+        for (var i = 0; i < response.data.length ; i++){
+     
+            $(".subHolder").append('<div class = "products"><h3>'+prod[i].productname+'</h3><p>'+prod[i].productdes+'</p><a id= "editP" name="'+prod[i].id+'" class="btn btn-primary" onclick = "edit(this.name)">Edit</a><a id = "deleteP" name="'+prod[i].id+'" class="btn btn-danger" onclick = "del(this.name)">Delete</a><hr></div>');
+        }
+    });
+
+});
+
+function edit(name){
+    console.log(name)
+    $("#formUpd").show();
+    $("#formAdd").hide();
+
+    var productname = document.getElementById('productname');
+    var productdes = document.getElementById('productdes')
+
+    axios.get('http://localhost:8000/api/product/show/'+name+'')
+    .then((response) => {
+        console.log(response)
+
+        productname.value = response.data.productname;
+        productdes.value = response.data.productdes;
+        var id = response.data.id;
+        formUpd.addEventListener('click', function() { 
+        
+            axios.put('http://localhost:8000/api/product/upd/'+id+'', {
+                    productname: productname.value,
+                    productdes: productdes.value
+                })
+                .then(response => {
+                    console.log(response);
+                })
+                .catch(error => {
+                    console.log(err);
+                });
+                getCurrentWindow().reload()
+        });
+        
+
+    });
+}
+
+function del(name){
+
+        var id = name
+
+        var bodyFormData = new FormData();
+        bodyFormData.append('id', id);
+        console.log(id)
+   
+        axios({
+            method: 'get',
+            url: 'http://localhost:8000/api/product/del/'+id+'',
+            data: bodyFormData,
+            // dataType: "json",
+            headers: {'Content-Type': 'multipart/form-data' }
+            })
+    
+            .then(function (response) {
+              
+                console.log(response.data.message)
+          
+                getCurrentWindow().reload()
+          
+            })
+            .catch(function (error) {
+    
+                console.log(error);
+            }); 
+}
